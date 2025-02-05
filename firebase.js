14d25322acc78a671fc9ea79d4024ae8e2a4b668
anish gehlot <anishgehlot25@gmail.com>
2025-02-05 11:54:23 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDkUPr0CZbuGKNjnstvHMga_0pHrB7Lurw",
+  authDomain: "infinity-solution-server.firebaseapp.com",
+  projectId: "infinity-solution-server",
+  storageBucket: "infinity-solution-server.firebasestorage.app",
+  messagingSenderId: "510140557040",
+  appId: "1:510140557040:web:341d07dd0322f4f2f30435"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
